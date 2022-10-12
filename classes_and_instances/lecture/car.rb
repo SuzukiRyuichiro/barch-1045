ad3d091657fffe5fc8517c48486fd14a9e4115ec
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-10-13 01:30:46 +0200

    define initialize of car class

--- classes_and_instances/lecture/car.rb
+++ classes_and_instances/lecture/car.rb
@@ -1,2 +1,9 @@
 class Car # mold / factory of cars
+  def initialize(color, brand)
+    # This is called on Car.new
+    # Here, we assign state / data / attributes of the insatnce
+    @color = color
+    @brand = brand
+    @engine_started = false
+  end
 end
