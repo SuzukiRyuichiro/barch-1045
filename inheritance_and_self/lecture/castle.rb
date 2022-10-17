5cb2a38f84e6e2658fd8e1da494607492df39dd6
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-10-17 12:03:38 +0200

    revised for the mornign lec

--- inheritance_and_self/lecture/castle.rb
+++ inheritance_and_self/lecture/castle.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 require_relative 'building'
+require_relative 'butler'
 
 class Castle < Building
   attr_accessor :butler
+
+  # When initializing, we want to assign a new butler automatically
+  def initialize(name, width, length, ruler)
+    super(name, width, length)
+    @ruler = ruler
+    @butler = Butler.new(self) # We have to give an instance of a castle
+  end
 
   # We want to add a butler (instance variable) to castle
   # Check if a castle has a butler
@@ -14,12 +22,21 @@ class Castle < Building
   # A castle always has a garden of 300 sq. m
   # castle.floor_area -> width * length + 300
   def floor_area
-    puts "hey, we're calling super!!!"
     super + 300
   end
 
   # Class method.
   def self.categories
     %w[Medieval Norman Ancient]
+  end
+
+  def ownership_details
+    "#{@name} is ruled by #{ruler_name}"
+  end
+
+  private
+
+  def ruler_name
+    @ruler.capitalize
   end
 end
